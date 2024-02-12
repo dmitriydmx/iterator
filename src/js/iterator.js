f194aedcfb47748e453b01cf53817165dfcc5b8f
dmitriydmx <dmitriydmx@mail.ru>
2024-02-12 18:16:07 +0900

    second commit iterator

--- src/js/iterator.js
+++ src/js/iterator.js
@@ -1,28 +1,50 @@
-import Team from './team';
+class Character {
+  constructor(name, type, health, level, attack, defence) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+  }
+}
+
+class Team {
+  constructor() {
+    this.characters = [];
+  }
+
+  [Symbol.iterator]() {
+    let index = 0;
+    const characters = this.characters;
+    return {
+      next() {
+        if (index < characters.length) {
+          return { value: characters[index++], done: false };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+
+  add(character) {
+    if (character instanceof Character) {
+      this.characters.push(character);
+    } else {
+      throw new Error('Only instances of Character can be added to the team');
+    }
+  }
+}
+
+
+const char1 = new Character('Лучник1', 'Bowman', 50, 1, 40, 10);
+const char2 = new Character('Лучник2', 'Bowman', 50, 1, 40, 10);
 
 const team = new Team();
-
-const char1 = {
-  name: 'Лучник',
-  type: 'Bowman',
-  health: 50,
-  level: 1,
-  attack: 40,
-  defence: 10
-};
-
-const char2 = {
-  name: 'Воин',
-  type: 'Swordsman',
-  health: 80,
-  level: 2,
-  attack: 65,
-  defence: 20
-};
-
 team.add(char1);
 team.add(char2);
 
-for (const character of team) {
+for (let character of team) {
   console.log(character);
 }
